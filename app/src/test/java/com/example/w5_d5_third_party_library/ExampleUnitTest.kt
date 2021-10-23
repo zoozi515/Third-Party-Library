2902440eac866c1d24e9aa8cd52aa991e3778f2f
Zahra Yousef <45607455+zoozi515@users.noreply.github.com>
2021-10-24 02:55:56 +0300

    Third Party Library - Version #1

---
+++ app/src/test/java/com/example/w5_d5_third_party_library/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.w5_d5_third_party_library
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
